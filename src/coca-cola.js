10685c358ba1146289e8dc005f51b35d301dfe5d
Hoolean <me@hoolean.com>
2015-01-10 12:35:55 +0000

    Added simple debug messages to ensure that the game loop is functioning correctly

--- src/coca-cola.js
+++ src/coca-cola.js
@@ -3,13 +3,29 @@ var thomas = require('thomas');
 function CocaCola(display)
 {
   this.display = display;
-  this.game = new thomas.Game();
-  this.loop = new thomas.Loop(this.game.callUpdate.bind(this.game), this.game.callRender.bind(this.game));
+  this.loop = new thomas.Loop(this.callUpdate.bind(this.game), this.callRender.bind(this.game));
 }
+
+CocaCola.prototype = Object.create(thomas.Game);
 
 CocaCola.prototype.start = function ()
 {
   this.loop.start();
+}
+
+CocaCola.prototype.begin = function ()
+{
+  console.log('Game Started');
+}
+
+CocaCola.prototype.update = function ()
+{
+  console.log('Game Update');
+}
+
+CocaCola.prototype.render = function ()
+{
+  console.log('Game Render');
 }
 
 module.exports = exports = CocaCola;
